81a85eb54c23f08f9f059a84940bd603a1d00622
Emin Koroglu <emin500@cs.utexas.edu>
2023-09-28 22:25:57 -0500

    fixed typo

--- matrix.c
+++ matrix.c
@@ -42,51 +42,36 @@ static Matrix matrix_subtract(Matrix res, const Matrix a, const Matrix b)
 static Matrix matrix_multiply(Matrix res, const Matrix a, const Matrix b)
 {
     // TODO: CHANGE THIS.
-    // double S1 = a.values[1][0] + a.values[1][1];
-    // double S2 = S1 - a.values[0][0];
-    // double S3 = a.values[0][0] - a.values[1][0];
-    // double S4 = a.values[0][1] - S2;
+    double S1 = a.values[1][0] + a.values[1][1];
+    double S2 = S1 - a.values[0][0];
+    double S3 = a.values[0][0] - a.values[1][0];
+    double S4 = a.values[0][1] - S2;
 
-    // double T1 = b.values[0][1] - b.values[0][0];
-    // double T2 = b.values[1][1] - T1;
-    // double T3 = b.values[1][1] - b.values[0][1];
-    // double T4 = b.values[1][0] - T2;
+    double T1 = b.values[0][1] - b.values[0][0];
+    double T2 = b.values[1][1] - T1;
+    double T3 = b.values[1][1] - b.values[0][1];
+    double T4 = b.values[1][0] - T2;
 
-    // double P1 = a.values[0][0] * b.values[0][0];
-    // double P2 = a.values[0][1] * b.values[1][0];
-    // double P3 = S1 * T1;
-    // double P4 = S2 * T2;
-    // double P5 = S3 * T3;
-    // double P6 = S4 * b.values[1][1];
-    // double P7 = a.values[1][1] * T4;
+    double P1 = a.values[0][0] * b.values[0][0];
+    double P2 = a.values[0][1] * b.values[1][0];
+    double P3 = S1 * T1;
+    double P4 = S2 * T2;
+    double P5 = S3 * T3;
+    double P6 = S4 * b.values[1][1];
+    double P7 = a.values[1][1] * T4;
 
-    // double U1 = P1 + P1;
-    // double U2 = P1 + P4;
-    // double U3 = U2 + P5;
-    // double U4 = U3 + P7;
-    // double U5 = U3 + P3;
-    // double U6 = U2 + P3;
-    // double U7 = U6 + P6;
+    double U1 = P1 + P2;
+    double U2 = P1 + P4;
+    double U3 = U2 + P5;
+    double U4 = U3 + P7;
+    double U5 = U3 + P3;
+    double U6 = U2 + P3;
+    double U7 = U6 + P6;
 
-    // res.values[0][0] = U1;
-    // res.values[0][1] = U7;
-    // res.values[1][0] = U4;
-    // res.values[1][1] = U5;
-  
-    // return res;
-
-    double m1 = (a.values[0][0] + a.values[1][1]) * (b.values[0][0] + b.values[1][1]);
-    double m2 = (a.values[1][0] + a.values[1][1]) *  b.values[0][0];
-    double m3 = (b.values[0][1] - b.values[1][1]) *  a.values[0][0];
-    double m4 = (b.values[1][0] - b.values[0][0]) *  a.values[1][1];
-    double m5 = (a.values[0][0] + a.values[0][1]) *  b.values[1][1];
-    double m6 = (a.values[1][0] - a.values[0][0]) * (b.values[0][0] + b.values[0][1]);
-    double m7 = (a.values[0][1] - a.values[1][1]) * (b.values[1][0] + b.values[1][1]);
-
-    res.values[0][0] = m1 + m4 - m5 + m7;
-    res.values[0][1] = m3 + m5;
-    res.values[1][0] = m2 + m4;
-    res.values[1][1] = m1 - m2 + m3 + m6;
+    res.values[0][0] = U1;
+    res.values[0][1] = U7;
+    res.values[1][0] = U4;
+    res.values[1][1] = U5;
   
     return res;
 }
